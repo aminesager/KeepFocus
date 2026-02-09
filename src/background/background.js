e2afde0690bde04cc08f516e8602606b991a7243
falcao <amine.saguer1@gmail.com>
2026-02-09 09:48:53 +0100

    refactor background script and add store manager - refactor background script to use store manager for various tasks - initializeDefaultSettings, add configs and more

--- src/background/background.js
+++ src/background/background.js
@@ -1,19 +1,6 @@
 importScripts("schedule.js");
 importScripts("bg-utils.js");
-
-const DEFAULT_SITES = [
-  "facebook.com",
-  "instagram.com",
-  "youtube.com",
-  "twitter.com",
-  "x.com",
-];
-
-chrome.storage.local.get(["BAD_SITES"], (data) => {
-  BAD_SITES = data.BAD_SITES || [];
-  console.log("Loaded:", BAD_SITES);
-  updateDNRRules();
-});
+importScripts("store-manager.js");
 
 chrome.runtime.onInstalled.addListener(async () => {
   console.log("Extension installed/updated");
@@ -31,104 +18,9 @@ async function isAdultBlockingActive() {
   }
 }
 
-async function initializeDefaultSettings() {
-  try {
-    const data = await chrome.storage.local.get([
-      "blockedSites",
-      "redirectUrl",
-      "breakEnabled",
-      "breakStartTime",
-      "breakEndTime",
-      "pauseEnabled",
-      "pauseStartTime",
-      "pauseEndTime",
-      "breakDays",
-      "defaultSites",
-      "adultEnabled",
-    ]);
-
-    // Initialize default sites
-    if (!data.defaultSites) {
-      const defaultSitesState = {};
-      DEFAULT_SITES.forEach((site) => {
-        defaultSitesState[site] = true;
-      });
-      await chrome.storage.local.set({ defaultSites: defaultSitesState });
-      console.log("Default sites initialized");
-    }
-    // Initialize other settings
-    if (!data.redirectUrl) {
-      await chrome.storage.local.set({ redirectUrl: "" });
-    }
-    if (!data.breakDays) {
-      await chrome.storage.local.set({ breakDays: [1, 2, 3, 4, 5] });
-    }
-    const defaults = {
-      breakEnabled: false,
-      pauseEnabled: false,
-      breakStartTime: "12:00",
-      breakEndTime: "13:00",
-      pauseStartTime: "18:00",
-      pauseEndTime: "08:00",
-      blockedSites: [],
-    };
-    const updates = {};
-    for (const [key, value] of Object.entries(defaults)) {
-      if (data[key] === undefined) {
-        updates[key] = value;
-      }
-    }
-    if (Object.keys(updates).length > 0) {
-      await chrome.storage.local.set(updates);
-      console.log("Default settings initialized:", updates);
-    }
-  } catch (error) {
-    console.error("Error initializing settings:", error);
-  }
-}
-
 chrome.runtime.onStartup.addListener(() => {
   console.log("Browser started, updating rules");
   updateDNRRules();
-});
-
-chrome.storage.onChanged.addListener((changes, namespace) => {
-  if (namespace === "local") {
-    console.log("Storage changed:", changes);
-    // Update DNR rules when relevant settings change
-    const relevantKeys = [
-      "blockedSites",
-      "defaultSites",
-      "redirectUrl",
-      "breakEnabled",
-      "pauseEnabled",
-      "adultEnabled",
-      "breakStartTime",
-      "breakEndTime",
-      "pauseStartTime",
-      "pauseEndTime",
-      "breakDays",
-    ];
-    const shouldUpdateRules = Object.keys(changes).some((key) =>
-      relevantKeys.includes(key),
-    );
-    if (shouldUpdateRules) {
-      console.log("Settings changed that affect blocking rules, updating...");
-      updateDNRRules();
-    }
-    // Notify content scripts about siteOptions changes
-    if (changes.siteOptions) {
-      chrome.tabs.query({}, (tabs) => {
-        tabs.forEach((tab) => {
-          if (tab.url && isSupportedSite(tab.url)) {
-            chrome.tabs.sendMessage(tab.id, {
-              action: "updateContentFiltering",
-            });
-          }
-        });
-      });
-    }
-  }
 });
 
 async function updateDNRRules() {
